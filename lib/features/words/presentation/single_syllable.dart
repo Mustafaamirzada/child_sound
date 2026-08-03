c11330d6d2833a179d131dfc660e7fb289f1b975
MustafaAmirzada <mustafa.amirzada.afg@gmail.com>
2026-08-03 17:54:14 +0430

    edit to use storage keys of app

--- lib/features/words/presentation/single_syllable.dart
+++ lib/features/words/presentation/single_syllable.dart
@@ -1,3 +1,4 @@
+import 'package:child_sound/core/services/storage_keys.dart';
 import 'package:child_sound/features/words/data/words_list.dart';
 import 'package:child_sound/features/words/presentation/widgets/word_list_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class SingleSyllableScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WordListScreen(
       title: "کلمات یک هجایی",
+      storageKey: StorageKeys.wordLevelOne,
       words: oneSyllableWords,
       gradientColors: [Colors.orange.shade300, Colors.deepOrange],
     );
--- lib/features/words/presentation/two_syllable.dart
+++ lib/features/words/presentation/two_syllable.dart
@@ -1,3 +1,4 @@
+import 'package:child_sound/core/services/storage_keys.dart';
 import 'package:child_sound/features/words/data/words_list.dart';
 import 'package:child_sound/features/words/presentation/widgets/word_list_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class TwoSyllableScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WordListScreen(
       title: "کلمات دو هجایی",
+      storageKey: StorageKeys.wordLevelTwo,
       words: twoSyllableWords,
       gradientColors: [Colors.blue.shade300, Colors.blue],
     );
